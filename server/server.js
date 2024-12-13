0ee7c7ffe814f6fd50d249adfe8b00ac469ed5ef
“G-Gakii” <gakiiviolet1@gmail.com>
2024-12-13 16:05:04 +0300

    add connection and disconnect event listeners

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "/../public");
 const port = 3000;
 const app = express();
+let server = http.createServer(app);
+
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log(`A new user just connected`);
+
+  socket.on("disconnect", () => {
+    console.log("User was disconnected from  server");
+  });
+});
+
+server.listen(port, () => {
   console.log(`server listening at port ${port}`);
 });
